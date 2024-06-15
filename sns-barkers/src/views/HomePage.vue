63ab6bf7d7b9c641612a34dc0fda6a63ed88172a
JOHS\zlwlw <johs970917@gmail.com>
2024-06-16 05:43:29 +0900

    필요없는 부분 제거

---
+++ sns-barkers/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>홈 페이지</h1>
+    <router-link to="/login">로그인</router-link> |
+    <router-link to="/register">회원가입</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ sns-barkers/src/views/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>로그인 페이지</h1>
+    <form>
+      <div>
+        <input type="text" placeholder="이메일" />
+      </div>
+      <div>
+        <input type="text" placeholder="비밀번호" />
+      </div>
+      <router-link to="/main">로그인</router-link>
+      <!-- <button type="submit">로그인</button> -->
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props) {},
+}
+</script>
+
+<style scoped></style>
