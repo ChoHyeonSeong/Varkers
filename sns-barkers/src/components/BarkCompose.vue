39b492016efdbf17bc17aeaf3a119d122ba6ef90
JOHS\zlwlw <johs970917@gmail.com>
2024-06-17 05:17:14 +0900

    bark compose 작성

---
+++ sns-barkers/src/components/BarkCompose.vue
@@ -0,0 +1,33 @@
+<template>
+  <Teleport to="body">
+    <form @submit.prevent="registerBark">
+      <modal :show="showModal" @close="$emit('close')">
+        <template #header>
+          <h3>Bark Register</h3>
+        </template>
+        <template #body>
+          <input type="text" v-model="bark" />
+        </template>
+        <template #footer>
+          <button type="submit">Register</button>
+        </template>
+      </modal>
+    </form>
+  </Teleport>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Modal from './common/Modal.vue';
+
+const bark = ref('');
+const emit = defineEmits(['close']);
+const props = defineProps({
+  showModal: Boolean,
+});
+
+function registerBark() {
+  emit('close');
+  console.log(bark.value);
+}
+</script>
---
+++ sns-barkers/src/components/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>메인 헤더</h1>
+    <button @click="toggleBarkCompose">BARK</button>
+    <bark-compose :showModal="showBarkCompose" @close="toggleBarkCompose" />
+  </div>
+</template>
+
+<script setup>
+import BarkCompose from './BarkCompose.vue';
+import { ref } from 'vue';
+
+const showBarkCompose = ref(false);
+
+function toggleBarkCompose() {
+  showBarkCompose.value = !showBarkCompose.value;
+}
+</script>
+
+<style scoped></style>
