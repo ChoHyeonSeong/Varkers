b0a740af918cca58430f06065fbcd89e0ba1d7ed
ChoHyeonSeong <johs970917@gmail.com>
2024-06-22 22:25:56 +0900

    Sse 컨트롤러 작성

---
+++ server/varkers/varkers/src/main/java/com/jhs/varkers/notification/NotificationService.java
@@ -0,0 +1,9 @@
+package com.jhs.varkers.notification;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface NotificationService {
+    SseEmitter subscribe(Long accountId);
+    SseEmitter createEmitter(Long accountId);
+    void sendToClient(Long accountId,Object data);
+}
---
+++ server/varkers/varkers/src/main/java/com/jhs/varkers/notification/NotificationServiceImpl.java
@@ -0,0 +1,51 @@
+package com.jhs.varkers.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.yaml.snakeyaml.emitter.Emitter;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private static final Long DEFAULT_TIMEOUT = 36000000L;
+    private final EmitterRepository emitterRepo;
+
+    @Override
+    public SseEmitter subscribe(Long accountId) {
+        SseEmitter emitter = createEmitter(accountId);
+        return null;
+    }
+
+    @Override
+    public void sendToClient(Long accountId, Object data) {
+        SseEmitter emitter = emitterRepo.findById(accountId);
+        if (emitter == null) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .id(String.valueOf(accountId))
+                        .name("sse")
+                        .data(data)
+                );
+
+            }catch (IOException e){
+                emitterRepo.deleteById(accountId);
+                emitter.completeWithError(e);
+            }
+        }
+    }
+
+    @Override
+    public SseEmitter createEmitter(Long accountId) {
+        SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
+        emitterRepo.save(accountId, emitter);
+
+        emitter.onCompletion(()->emitterRepo.deleteById(accountId));
+        emitter.onTimeout(()->emitterRepo.deleteById(accountId));
+
+        return emitter;
+    }
+}
